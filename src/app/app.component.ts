3658933da01e8bca4a4fe284b6ef750cbf35ce91
mark.collier <mark.collier@qtsus-MacBook-Pro.local>
2019-02-28 17:50:57 +0800

    Finished entire tutorial, Now trying https://angular.io/guide/displaying-data#showing-an-array-property-with-ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero}}</h2>
+  <p>Heroes:</p>
+  <ul>
+    <li *ngFor="let hero of heroes">
+      {{ hero }}
+    </li>
+  </ul>
+
+  `
+  /*
+  Notice the hero in the ngFor double-quoted instruction; it is an example of a template input variable.
+  Read more about template input variables in the microsyntax section of the Template Syntax page.
+  Angular duplicates the <li> for each item in the list, setting the hero variable to the item (the hero) in the current iteration.
+  Angular uses that variable as the context for the interpolation in the double curly braces.
+  In this case, ngFor is displaying an array, but ngFor can repeat items for any iterable object.
+  Now the heroes appear in an unordered list.
+  */
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = ['Windstorm','Bombasto','Magneta','Tornado'];
+  myHero = this.heroes[0];
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -102,10 +101,13 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
   /**
    * Handle Http operation that failed.
