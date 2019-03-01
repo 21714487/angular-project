775e565e02bb8e908d99f36639367c09d1de05fa
mark.collier <mark.collier@qtsus-MacBook-Pro.local>
2019-03-01 15:21:28 +0800

    Finished https://angular.io/guide/displaying-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './hero';
 @Component({
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero}}</h2>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
   <p>Heroes:</p>
   <ul>
     <li *ngFor="let hero of heroes">
-      {{ hero }}
+      {{ hero.name }}
     </li>
   </ul>
-
+<p *ngIf="heroes.length > 3">There are {{heroes.length}} heroes!</p>
   `
   /*
   Notice the hero in the ngFor double-quoted instruction; it is an example of a template input variable.
@@ -24,6 +24,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = ['Windstorm','Bombasto','Magneta','Tornado'];
+  heroes = [
+      new Hero(1, 'Windstorm'),
+      new Hero(13, 'Bombasto'),
+      new Hero(15, 'Magneta'),
+      new Hero(20, 'Tornado')
+    ];
   myHero = this.heroes[0];
 }
